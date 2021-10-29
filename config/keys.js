b372f7b2b3c99d2f6b45d3914c484929110d96dc
zahra <zahra.naqvi26@gmail.com>
2021-10-29 08:08:39 +0500

    First commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const dbPassword = "mongodb+srv://zhra:robertlangdon@sandbox.a1hut.mongodb.net/node_passport_login.one?retryWrites=true&w=majority";
+
+module.exports = {
+    mongoURI: dbPassword
+};
